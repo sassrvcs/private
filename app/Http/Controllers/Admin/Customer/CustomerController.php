9d00d0d00d50b2cd9bf1d4a1c6a6b8e50f89eff0
prattusighosh <prattusighosh@technoexponent.com>
2023-08-02 16:17:07 +0530

    customer list edit delete

--- app/Http/Controllers/Admin/Customer/CustomerController.php
+++ app/Http/Controllers/Admin/Customer/CustomerController.php
@@ -25,41 +25,7 @@ class CustomerController extends Controller
         //print_r($customerlist['address']);exit;
         return view('admin.customer.index',compact('customerlist'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('admin.accounting.create');
-    }
-    public function store(Request $request)
-    {
-        $input = $request->all();
-        //dd($input);
-        $validate = Validator::make($request->all(), [
-            'name' => 'required',
-            'short_desc' => 'required',
-            'description' => 'required',
-            'image' => 'required|image|mimes:jpg,png,jpeg,gif'
-            ],[
-                'name.required' =>'This name field is required.',
-                'short_desc.required' => 'This short description field is required.',
-                'description.required' => 'This description field is required.',
-                'image.required' => 'This image field is required and only allows image files.',
-                'image.image' => 'This image field is required and only allows image files.'
-            ]);
-        if($validate->fails()){
-            return back()->withErrors($validate->errors())->withInput();
-        }else{
-
-            $accountserviceId = $this->userService->store($input);
-
-            //return redirect()->back()->with('message', 'Accounting software added successfully');
-            return Redirect::to("admin/accounting")->withSuccess('Accounting Software Added Successfully');
-        }
-
-    }
+    
 
     /**
      *
@@ -68,28 +34,41 @@ class CustomerController extends Controller
      */
     public function edit(string $id)
     {
-        $accounting = $this->userService->edit($id);
-        return view('admin.accounting.edit',compact('accounting'));
+        $customerDetails = $this->userService->edit($id);
+        return view('admin.customer.edit',compact('customerDetails'));
     }
 
     public function update(Request $request, string $id)
     {
         $input = $request->all();
         $validate = Validator::make($request->all(), [
-            'name' => 'required',
-            'short_desc' => 'required',
-            'description' => 'required'
+            'title' => 'required',
+            'forename' => 'required',
+            'surname' => 'required',
+            'phone' => 'required',
+            'email' => 'required|email',
+            'house_number' => 'required',
+            'street' => 'required',
+            'town' => 'required',
+            'postcode' => 'required'
             ],[
-                'name.required' =>'This name field is required.',
-                'short_desc.required' => 'This short description field is required.',
-                'description.required' => 'This description field is required.'
+                'title.required' =>'This title field is required.',
+                'forename.required' => 'This forename field is required.',
+                'surname.required' => 'This surname field is required.',
+                'phone.required' =>'This phone field is required.',
+                'email.required' => 'This email field is required.',
+                'email.email' => 'This email field should be an valid email type.',
+                'house_number.required' => 'This house number field is required.',
+                'street.required' => 'This street field is required.',
+                'town.required' => 'This town field is required.',
+                'postcode.required' => 'This postcode field is required.',
             ]);
         if($validate->fails()){
             return back()->withErrors($validate->errors())->withInput();
         }else{
             $user = $this->userService->update($input,$id);
             if($user){
-                return Redirect::to("admin/accounting")->withSuccess('Accounting Software updated');
+                return Redirect::to("admin/customer")->withSuccess('Customer Details updated');
             }
 
         }
@@ -97,9 +76,8 @@ class CustomerController extends Controller
 
     public function destroy(string $id)
     {
-        $accounting = $this->userService->destroy($id);
-        if($accounting) {
-            sleep(2);
+        $customer = $this->userService->destroy($id);
+        if($customer) {
             return 1;
         } else {
             return 0;
--- resources/views/admin/customer/edit.blade.php
+++ resources/views/admin/customer/edit.blade.php
@@ -1,18 +1,18 @@
 @extends('includes.layouts.admin')
 @section('page-title')
-    Edit Accounting Software
+    Edit Custome
 @endsection
 @section('content')
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1> Edit Accounting Software </h1>
+                    <h1> Edit Custome </h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
-                        <li class="breadcrumb-item active">Edit Accounting Software </li>
+                        <li class="breadcrumb-item active">Edit Custome </li>
                     </ol>
                 </div>
             </div>
@@ -25,53 +25,96 @@
 
                     <div class="card card-primary">
                         <div class="card-body">
-                            <form action="{{ route('admin.accounting.update', $accounting->id) }}" method="POST" enctype="multipart/form-data">
+                            <form action="{{ route('admin.customer.update', $customerDetails->id) }}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 @method('PUT')
                                 <div class="row">
-                                    <div class="col-sm-6">
-                                        <x-Forms.Input type="text" mandate="*" label="Name" id="name"
-                                            name="name" value="{{ $accounting->accounting_software_name }}"
-                                            placeholder="Enter Accounting Software Name"
-                                            class="{{ $errors->has('name') ? 'is-invalid' : '' }}" />
+                                    <div class="col-sm-4"> 
+                                        <label>Title<span class="mandetory">*</span></label>   
+                                        <select class="select form-control @error('title') is-invalid @enderror" name="title" value={{old('title')}}>
+                                            <option value="Mr" @if($customerDetails->title == "Mr") selected @endif>Mr</option>
+                                            <option value="Mrs" @if($customerDetails->title == "Mrs") selected @endif>Mrs</option>
+                                            <option value="Miss" @if($customerDetails->title == "Miss") selected @endif>Miss</option>
+                                            <option value="Sir" @if($customerDetails->title == "Sir") selected @endif>Sir</option>
+                                            <option value="Ms" @if($customerDetails->title == "Ms") selected @endif>Ms</option>
+                                            <option value="Dr" @if($customerDetails->title == "Dr") selected @endif>Dr</option>
+                                            <option value="Madam" @if($customerDetails->title == "Madam") selected @endif>Madam</option>
+                                            <option value="Ma'am" @if($customerDetails->title == "Ma'am") selected @endif>Ma'am</option>
+                                            <option value="Lord" @if($customerDetails->title == "Lord") selected @endif>Lord</option>
+                                            <option value="Lady" @if($customerDetails->title == "Lady") selected @endif>Lady</option>
+                                        </select>
+                                        @error('title')
+                                            <div class="error" style="color:red;">{{ $message }}</div>
+                                        @enderror
+                                    
                                     </div>
 
-                                    <div class="col-sm-6">
-                                        <x-Forms.Input type="text" mandate="*" label="Short Description" id="short_desc"
-                                            name="short_desc" value="{{ $accounting->short_desc }}"
-                                            class="{{ $errors->has('short_desc') ? 'is-invalid' : '' }}"  />
+                                    <div class="col-sm-4">
+                                        <x-Forms.Input type="text" name="forename" id="forename" mandate="*" label="Forename" value="{{ $customerDetails->forename }}" class="form-control {{ $errors->has('forename') ? 'is-invalid' : '' }}"  />
                                     </div>
 
-                                </div>
+                                    <div class="col-sm-4">
+                                        <x-Forms.Input type="text" name="surname" id="surname" mandate="*" label="Surname" value="{{ $customerDetails->surname }}" class="form-control {{ $errors->has('surname') ? 'is-invalid' : '' }}"  />
+                                    </div>
 
+                                </div>    
 
                                 <div class="row">
-                                    <div class="col-sm-12">
-                                        <label for="">Description <span class="mandetory">*</span></label>
-                                        <textarea class="ckeditor form-control {{ $errors->has('description') ? 'is-invalid' : '' }}" name="description">
-                                            {!! $accounting->long_desc !!}
-                                        </textarea>
-                                        <span class="error invalid-feedback">{{ $errors->first('description') }}</span>
+
+                                    <div class="col-sm-4">
+                                        <x-Forms.Input type="text" name="phone" id="phone" mandate="*" label="Phone" value="{{ $customerDetails->phone_no }}" class="form-control {{ $errors->has('phone') ? 'is-invalid' : '' }}"  />
                                     </div>
+
+                                    <div class="col-sm-4">
+                                        <x-Forms.Input type="email" name="email" id="email" mandate="*" label="Email" value="{{ $customerDetails->email }}" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"  />
+                                    </div>
+
+                                    <div class="col-sm-4">
+                                        <x-Forms.Input type="text" name="organisation" id="organisation"  label="Organisation" value="{{ $customerDetails->organisation }}" />
+                                    </div>
+                                    
                                 </div>
 
-                                <div class="row">
-                                    <div class="col-sm-8">
-                                        <div class="upload-img">
-                                            <span>    
-                                            <?php $stored_image = $accounting->image ;?>
-                                            <img id="preview-image-before-upload" src="<?= url("/images/$stored_image") ?>" alt="Image" width="50" height="50"/>
-                                            </span>
-                                            
-                                            <x-Forms.Input type="file" mandate="*" label="Image" name="image" id="image"
-                                            class="{{ $errors->has('image') ? 'is-invalid' : '' }}" />
-                                        </div>
-                                    </div>
-                                </div>
                                 
+                                <div><span style="font-weight:bold">Primary Address:</span></div>
+
+
+                                <!--address section start-->
+                                <div class="row">
+                                    <div class="col-sm-4">
+                                        <x-Forms.Input type="text" name="house_number" id="house_number" mandate="*" label="House Name/Number" value="{{ $customerDetails['address']->house_number }}" class="form-control {{ $errors->has('house_number') ? 'is-invalid' : '' }}"  />
+                                    </div>
+
+                                    <div class="col-sm-4">
+                                        <x-Forms.Input type="text" name="street" id="street" mandate="*" label="Street" value="{{ $customerDetails['address']->street }}" class="form-control {{ $errors->has('street') ? 'is-invalid' : '' }}"  />
+                                    </div>
+
+                                    <div class="col-sm-4">
+                                        <x-Forms.Input type="text" name="locality" id="locality" label="Locality" value="{{ $customerDetails['address']->locality }}"/>
+                                    </div>
+
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-sm-4">
+                                        <x-Forms.Input type="text" name="town" id="town" mandate="*" label="Town " value="{{ $customerDetails['address']->town }}" class="form-control {{ $errors->has('town') ? 'is-invalid' : '' }}"  />
+                                    </div>
+
+                                    <div class="col-sm-4">
+                                        <x-Forms.Input type="text" name="country" id="country" label="Country" value="{{ $customerDetails['address']->country }}"/>
+                                    </div>
+
+                                    <div class="col-sm-4">
+                                        <x-Forms.Input type="text" name="postcode" id="postcode" mandate="*" label="Postcode" value="{{ $customerDetails['address']->post_code }}" class="form-control {{ $errors->has('postcode') ? 'is-invalid' : '' }}"/>
+                                    </div>
+
+                                </div>
+                                <!--address section start-->
+
                                 <button class="btn btn_baseColor btn-sm mt-2" type="submit"
                                     onClick="this.form.submit(); this.disabled=true; this.innerText='Hold on...';"> &nbsp;&nbsp; Save &nbsp;&nbsp;
                                 </button>
+                                   
                             </form>
                         </div>
                     </div>
