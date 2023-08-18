a28b5a25016a0ff2d3ed0bcdd536555419ba24f2
sudeep <sudeepchowdhury@technoexponent.co.in>
2023-08-18 17:50:49 +0530

    sic section

--- config/sic_code.php
+++ config/sic_code.php
@@ -90,30 +90,454 @@ return [
     'sic_code' => [
         "1" => [
             '1' => [
-                "id"   => "1110",
-                "name" => 'Growing of cereals (except rice), leguminous crops and oil seeds'
+                "id"   => "56302",
+                "name" => 'Public houses and bars'
             ],
             '2' => [
-                "id"   => "1120",
-                "name" => 'Growing of rice'
+                "id"   => "56301",
+                "name" => 'Licenced clubs'
+            ],
+            '3' => [
+                "id"   => "563290",
+                "name" => 'Other food services'
+            ],
+            '4' => [
+                "id"   => "56210",
+                "name" => 'Event catering activities'
+            ],
+            '5' => [
+                "id"   => "56103",
+                "name" => 'Take-away food shops and mobile food stands'
+            ],
+            '6' => [
+                "id"   => "56102",
+                "name" => 'Unlicenced restaurants and cafes'
+            ],
+            '7' => [
+                "id"   => "56101",
+                "name" => 'Licenced restaurants'
+            ],
+            '8' => [
+                "id"   => "55900",
+                "name" => 'Other accommodation'
+            ],
+            '9' => [
+                "id"   => "55300",
+                "name" => 'Recreational vehicle parks, trailer parks and camping grounds'
+            ],
+            '10' => [
+                "id"   => "55209",
+                "name" => 'Other holiday and other collective accommodation'
+            ],
+            '11' => [
+                "id"   => "55202",
+                "name" => 'Youth hostels'
+            ],
+            '12' => [
+                "id"   => "55201",
+                "name" => 'Holiday centres and villages'
+            ],
+            '13' => [
+                "id"   => "55100",
+                "name" => 'Hotels and similar accommodation'
             ]
         ],
         "2" => [
-            '3' => [
-                "id"   => "1130",
-                "name" => 'Growing of vegetables and melons, roots and tubers'
+            '14' => [
+                "id"   => "99999",
+                "name" => 'Dormant Company'
             ],
-            '4' => [
-                "id"   => "1140",
-                "name" => 'Growing of sugar cane'
+            '15' => [
+                "id"   => "99000",
+                "name" => 'Activities of extraterritorial organisations and bodies'
+            ]
+        ],
+        "3" => [
+            '16' => [
+                "id"   => "98200",
+                "name" => 'Undifferentiated service-producing activities of private households for own use'
             ],
-            '5' => [
-                "id"   => "1150",
+            '17' => [
+                "id"   => "98100",
+                "name" => 'Undifferentiated goods-producing activities of private households for own use'
+            ],
+            '18' => [
+                "id"   => "98000",
+                "name" => 'Residents property management'
+            ],
+            '19' => [
+                "id"   => "97000",
+                "name" => 'Activities of households as employers of domestic personnel'
+            ]
+        ],
+        "4" => [
+            '20' => [
+                "id"   => "82990",
+                "name" => 'Other business support service activities n.e.c.'
+            ],
+            '21' => [
+                "id"   => "82920",
+                "name" => 'Packaging activities'
+            ],
+            '22' => [
+                "id"   => "82912",
+                "name" => 'Activities of credit bureaus'
+            ],
+            '23' => [
+                "id"   => "82911",
+                "name" => 'Activities of collection agencies'
+            ],
+            '24' => [
+                "id"   => "82302",
+                "name" => 'Activities of conference organisers'
+            ],
+            '25' => [
+                "id"   => "82301",
+                "name" => 'Activities of exhibition and fair organisers'
+            ],
+            '26' => [
+                "id"   => "82200",
+                "name" => 'Activities of call centres'
+            ],
+            '27' => [
+                "id"   => "82190",
+                "name" => 'Photocopying, document preparation and other specialised office support activities'
+            ],
+            '28' => [
+                "id"   => "82110",
+                "name" => 'Combined office administrative service activities'
+            ],
+            '29' => [
+                "id"   => "81300",
+                "name" => 'Landscape service activities'
+            ],
+            '30' => [
+                "id"   => "81299",
+                "name" => 'Other cleaning services'
+            ],
+            '31' => [
+                "id"   => "81291",
+                "name" => 'Disinfecting and exterminating services'
+            ],
+            '32' => [
+                "id"   => "81229",
+                "name" => 'Other building and industrial cleaning activities'
+            ],
+            '33' => [
+                "id"   => "81223",
+                "name" => 'Furnace and chimney cleaning services'
+            ],
+            '34' => [
+                "id"   => "81222",
+                "name" => 'Specialised cleaning services'
+            ],
+            '35' => [
+                "id"   => "81221",
+                "name" => 'Window cleaning services'
+            ],
+            '36' => [
+                "id"   => "81210",
+                "name" => 'General cleaning of buildings'
+            ],
+            '37' => [
+                "id"   => "81100",
+                "name" => 'Combined facilities support activities'
+            ],
+            '38' => [
+                "id"   => "80300",
+                "name" => 'Investigation activities'
+            ],
+            '39' => [
+                "id"   => "80200",
+                "name" => 'Security systems service activities'
+            ],
+            '40' => [
+                "id"   => "80100",
+                "name" => 'Private security activities'
+            ],
+            '41' => [
+                "id"   => "79909",
+                "name" => 'Other reservation service activities n.e.c.'
+            ],
+            '42' => [
+                "id"   => "79901",
+                "name" => 'Activities of tourist guides'
+            ],
+            '43' => [
+                "id"   => "79120",
+                "name" => 'Tour operator activities'
+            ],
+            '44' => [
+                "id"   => "79110",
+                "name" => 'Travel agency activities'
+            ],
+            '45' => [
+                "id"   => "78300",
+                "name" => 'Human resources provision and management of human resources functions'
+            ],
+            '46' => [
+                "id"   => "78200",
+                "name" => 'Temporary employment agency activities'
+            ],
+            '47' => [
+                "id"   => "78109",
+                "name" => 'Other activities of employment placement agencies'
+            ],
+            '48' => [
+                "id"   => "78101",
+                "name" => 'Motion picture, television and other theatrical casting activities'
+            ],
+            '49' => [
+                "id"   => "77400",
+                "name" => 'Leasing of intellectual property and similar products, except copyright works'
+            ],
+            '50' => [
+                "id"   => "77390",
+                "name" => 'Renting and leasing of other machinery, equipment and tangible goods n.e.c.'
+            ],
+            '51' => [
+                "id"   => "77352",
+                "name" => 'Renting and leasing of freight air transport equipment'
+            ],
+            '52' => [
+                "id"   => "77351",
+                "name" => 'Renting and leasing of air passenger transport equipment'
+            ],
+            '53' => [
+                "id"   => "77342",
+                "name" => 'Renting and leasing of freight water transport equipment'
+            ],
+            '54' => [
+                "id"   => "77341",
+                "name" => 'Renting and leasing of passenger water transport equipment'
+            ],
+            '55' => [
+                "id"   => "77330",
+                "name" => 'Renting and leasing of office machinery and equipment (including computers)'
+            ],
+            '56' => [
+                "id"   => "77320",
+                "name" => 'Renting and leasing of construction and civil engineering machinery and equipment'
+            ],
+            '57' => [
+                "id"   => "77310",
+                "name" => 'Renting and leasing of agricultural machinery and equipment'
+            ],
+            '58' => [
+                "id"   => "77299",
+                "name" => 'Renting and leasing of other personal and household goods'
+            ],
+            '59' => [
+                "id"   => "77291",
+                "name" => 'Renting and leasing of media entertainment equipment'
+            ],
+            '60' => [
+                "id"   => "77220",
+                "name" => 'Renting of video tapes and disks'
+            ],
+            '61' => [
+                "id"   => "77210",
+                "name" => 'Renting and leasing of recreational and sports goods'
+            ],
+            '62' => [
+                "id"   => "77120",
+                "name" => 'Renting and leasing of trucks and other heavy vehicles'
+            ],
+            '63' => [
+                "id"   => "77110",
+                "name" => 'Renting and leasing of cars and light motor vehicles'
+            ]
+        ],
+        "5" => [
+            '64' => [
+                "id"   => "01250",
+                "name" => 'Growing of other tree and bush fruits and nuts'
+            ],
+            '17' => [
+                "id"   => "03220",
+                "name" => 'Freshwater aquaculture'
+            ],
+            '18' => [
+                "id"   => "03210",
+                "name" => 'Marine aquaculture'
+            ],
+            '19' => [
+                "id"   => "03120",
+                "name" => 'Freshwater fishing'
+            ],
+            '19' => [
+                "id"   => "03110",
+                "name" => 'Marine fishing'
+            ],
+            '19' => [
+                "id"   => "02400",
+                "name" => 'Support services to forestry'
+            ],
+            '19' => [
+                "id"   => "02300",
+                "name" => 'Gathering of wild growing non-wood products'
+            ],
+            '19' => [
+                "id"   => "02200",
+                "name" => 'Logging'
+            ],
+            '19' => [
+                "id"   => "02100",
+                "name" => 'Silviculture and other forestry activities'
+            ],
+            '19' => [
+                "id"   => "01700",
+                "name" => 'Hunting, trapping and related service activities'
+            ],
+            '19' => [
+                "id"   => "01640",
+                "name" => 'Freshwater fishingSeed processing for propagation'
+            ],
+            '19' => [
+                "id"   => "01630",
+                "name" => 'Post-harvest crop activities'
+            ],
+            '19' => [
+                "id"   => "01629",
+                "name" => 'Support activities for animal production (other than farm animal boarding and care) n.e.c.'
+            ],
+            '19' => [
+                "id"   => "01621",
+                "name" => 'Farm animal boarding and care'
+            ],
+            '19' => [
+                "id"   => "01610",
+                "name" => 'Support activities for crop production'
+            ],
+            '19' => [
+                "id"   => "01500",
+                "name" => 'Mixed farming'
+            ],
+            '19' => [
+                "id"   => "01490",
+                "name" => 'Raising of other animals'
+            ],
+            '19' => [
+                "id"   => "01470",
+                "name" => 'Raising of poultry'
+            ],
+            '19' => [
+                "id"   => "01460",
+                "name" => 'Raising of swine / pigs'
+            ],
+            '19' => [
+                "id"   => "01450",
+                "name" => 'Raising of sheep and goats'
+            ],
+            '19' => [
+                "id"   => "01440",
+                "name" => 'Raising of camels and camelids'
+            ],
+            '19' => [
+                "id"   => "01430",
+                "name" => 'Raising of horses and other equines'
+            ],
+            '19' => [
+                "id"   => "01420",
+                "name" => 'Raising of other cattle and buffaloes'
+            ],
+            '19' => [
+                "id"   => "01410",
+                "name" => 'Raising of dairy cattle'
+            ],
+            '19' => [
+                "id"   => "01300",
+                "name" => 'Plant propagation'
+            ],
+            '19' => [
+                "id"   => "01290",
+                "name" => 'Growing of other perennial crops'
+            ],
+            '19' => [
+                "id"   => "01280",
+                "name" => 'Growing of spices, aromatic, drug and pharmaceutical crops'
+            ],
+            '19' => [
+                "id"   => "01270",
+                "name" => 'Growing of beverage crops'
+            ],
+            '19' => [
+                "id"   => "01260",
+                "name" => 'Growing of oleaginous fruits'
+            ],
+            '19' => [
+                "id"   => "01240",
+                "name" => 'Growing of pome fruits and stone fruits'
+            ],
+            '19' => [
+                "id"   => "01230",
+                "name" => 'Growing of citrus fruits'
+            ],
+            '19' => [
+                "id"   => "01220",
+                "name" => 'Growing of tropical and subtropical fruits'
+            ],
+            '19' => [
+                "id"   => "01210",
+                "name" => 'Growing of grapes'
+            ],
+            '19' => [
+                "id"   => "01190",
+                "name" => 'Growing of other non-perennial crops'
+            ],
+            '19' => [
+                "id"   => "01160",
+                "name" => 'Growing of fibre crops'
+            ],
+            '19' => [
+                "id"   => "01150 ",
                 "name" => 'Growing of tobacco'
             ],
-            '6' => [
-                "id"   => "1151",
-                "name" => 'Growing of tobacco and sugar dummy datasets'
+            '19' => [
+                "id"   => "01140",
+                "name" => 'Growing of sugar cane'
+            ],
+            '19' => [
+                "id"   => "01130",
+                "name" => 'Growing of vegetables and melons, roots and tubers'
+            ],
+            '19' => [
+                "id"   => "01120",
+                "name" => 'Growing of rice'
+            ],
+            '19' => [
+                "id"   => "01110",
+                "name" => 'Growing of cereals (except rice), leguminous crops and oil seeds'
+            ]
+        ],
+        "6" => [
+            '14' => [
+                "id"   => "99999",
+                "name" => 'Dormant Company'
+            ],
+            '15' => [
+                "id"   => "99000",
+                "name" => 'Activities of extraterritorial organisations and bodies'
+            ]
+        ],
+        "7" => [
+            '14' => [
+                "id"   => "99999",
+                "name" => 'Dormant Company'
+            ],
+            '15' => [
+                "id"   => "99000",
+                "name" => 'Activities of extraterritorial organisations and bodies'
+            ]
+        ],
+        "8" => [
+            '14' => [
+                "id"   => "99999",
+                "name" => 'Dormant Company'
+            ],
+            '15' => [
+                "id"   => "99000",
+                "name" => 'Activities of extraterritorial organisations and bodies'
             ]
         ],
     ]
