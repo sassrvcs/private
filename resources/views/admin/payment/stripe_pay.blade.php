4f84bc1099d7119b5382c667abdbae26a37a21ef
ChanchalBansal <chanchal2895@gmail.com>
2025-12-11 11:13:26 +0530

    stripe changes

--- resources/views/admin/payment/stripe_pay.blade.php
+++ resources/views/admin/payment/stripe_pay.blade.php
@@ -71,7 +71,7 @@ $(document).ready(function(){
         theme: "bootstrap-5",
         width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
         placeholder: $( this ).data( 'placeholder' ),
-        closeOnSelect: false,
+        closeOnSelect: true,
     });
 });
 </script>
