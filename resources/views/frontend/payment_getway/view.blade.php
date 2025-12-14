864eb753cfd036319d6b8d6f24847461d49c98f4
ChanchalBansal <chanchal2895@gmail.com>
2025-12-14 15:51:52 +0530

    stripe changes

--- resources/views/frontend/payment_getway/view.blade.php
+++ resources/views/frontend/payment_getway/view.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.app')
 @section('content')
-    <!-- <form action="{{ $paymentUrl }}" id="payment_getway" method="POST">
-        @foreach ($formData as $key => $value)
-            <input type="text" name="{{ $key }}" value="{{ $value }}">
-        @endforeach
+    <!-- <form action="" id="payment_getway" method="POST">
+        <input type="text" name="" value="">
 
     </form> -->
 @endsection
