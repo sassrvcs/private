816913bca625cec2beba74b69b2aa21581449623
ChanchalBansal <chanchal2895@gmail.com>
2025-12-15 11:32:52 +0530

    stripe changes

--- database/migrations/2023_11_02_063734_add_desc_to_facilities_table.php
+++ database/migrations/2023_11_02_063734_add_desc_to_facilities_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('facilities', function (Blueprint $table) {
-            $table->longText("description");
+            $table->longText("description")->nullable();
         });
     }
 
